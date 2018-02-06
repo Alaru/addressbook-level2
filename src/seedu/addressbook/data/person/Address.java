9d0bece32937f67a5bfb218d6b1327c53de58d13
Ellery Chia <ellerychia@msn.com>
2018-02-06 23:55:00 +0800

    W4.4b: Updated Address.java to utilise the 4 classes

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,11 +8,17 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses must follow the format {Block, Street," +
+                                                                " Unit Number, Postal Code)";
+    public static final String ADDRESS_VALIDATION_REGEX = "\\d+, .+, .+, \\d+";
+    private static final String ADDRESS_DELIMITER = ", ";
 
     public final String value;
+    private Block block;
+    private Street street;
+    private Unit unit;
+    private PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -26,8 +32,29 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
+        createAddress(trimmedAddress);
         this.value = trimmedAddress;
     }
+
+    /**
+     * Splits the address into the 4 parts and initializes the objects
+     * @param address argument supplied by user
+     */
+    private void createAddress(String address) {
+        String[] addressValues = address.split(ADDRESS_DELIMITER);
+        block = new Block(addressValues[0]);
+        street = new Street(addressValues[1]);
+        unit = new Unit(addressValues[2]);
+        postalCode = new PostalCode(addressValues[3]);
+    }
+
+    private Block getBlock() { return block; }
+
+    private Street getStreet() { return street; }
+
+    private Unit getUnit() { return unit; }
+
+    private PostalCode getPostalCode() { return postalCode; }
 
     /**
      * Returns true if a given string is a valid person address.
@@ -45,7 +72,10 @@ public class Address {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.block.equals(((Address) other).getBlock())
+                && this.street.equals(((Address) other).getStreet())
+                && this.unit.equals(((Address) other).getUnit())
+                && this.postalCode.equals(((Address) other).getPostalCode()));
     }
 
     @Override

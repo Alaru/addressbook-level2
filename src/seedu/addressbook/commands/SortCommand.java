51dc7fc51b6bac1aeb65e86cb2a670393112d78b
Ellery Chia <ellerychia@msn.com>
2018-02-16 02:24:07 +0800

    Update Parser to call SortCommand class

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,11 +2,11 @@ package seedu.addressbook.commands;
 
 
 /**
- * Lists all persons in the address book to the user.
+ * Sorts all Persons in the address book to the user.
  */
 public class SortCommand extends seedu.addressbook.commands.Command {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Displays all persons in the address book as a list with index numbers.\n"
